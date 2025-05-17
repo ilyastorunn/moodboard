517486dcf7dbf412113b5a802eb36dc1228458e4
İlyas Torun <your-email@example.com>
2025-05-18 02:29:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import Music from './Pages/Music'
+
+function App() {
+  return (
+    <>
+      <Music />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Music.jsx
@@ -0,0 +1,59 @@
+import album1 from "../assets/albums/1.jpg";
+import album2 from "../assets/albums/2.jpeg";
+import album3 from "../assets/albums/3.jpg";
+import album4 from "../assets/albums/4.jpg";
+import album5 from "../assets/albums/5.png";
+import album6 from "../assets/albums/6.png";
+import album7 from "../assets/albums/7.webp";
+import album8 from "../assets/albums/8.webp";
+import album9 from "../assets/albums/9.jpg";
+import album10 from "../assets/albums/10.webp";
+import album11 from "../assets/albums/11.jpeg";
+import album12 from "../assets/albums/12.jpg";
+import album13 from "../assets/albums/13.jpg";
+import album14 from "../assets/albums/14.jpg";
+import album15 from "../assets/albums/15.jpeg";
+import album16 from "../assets/albums/16.jpeg";
+import album17 from "../assets/albums/17.jpg";
+import album18 from "../assets/albums/18.jpg";
+import album19 from "../assets/albums/19.jpg";
+import album20 from "../assets/albums/20.jpg";
+import album21 from "../assets/albums/21.jpg";
+import album22 from "../assets/albums/22.png";
+import album23 from "../assets/albums/23.webp";
+import album24 from "../assets/albums/24.png";
+import album25 from "../assets/albums/25.webp";
+
+export default function Music() {
+  return (
+    <div className="bg-[#F7F7F7] flex items-center h-screen overflow-x-auto">
+      <div className="flex min-w-max ml-[160px]">
+        <img src={album1} alt="" className="w-[200px] h-[200px] hover:rounded-full" />
+        <img src={album2} alt="" className="w-[200px] h-[200px]" />
+        <img src={album3} alt="" className="w-[200px] h-[200px]" />
+        <img src={album4} alt="" className="w-[200px] h-[200px]" />
+        <img src={album5} alt="" className="w-[200px] h-[200px]" />
+        <img src={album6} alt="" className="w-[200px] h-[200px]" />
+        <img src={album7} alt="" className="w-[200px] h-[200px]" />
+        <img src={album8} alt="" className="w-[200px] h-[200px]" />
+        <img src={album9} alt="" className="w-[200px] h-[200px]" />
+        <img src={album10} alt="" className="w-[200px] h-[200px]" />
+        <img src={album11} alt="" className="w-[200px] h-[200px]" />
+        <img src={album12} alt="" className="w-[200px] h-[200px]" />
+        <img src={album13} alt="" className="w-[200px] h-[200px]" />
+        <img src={album14} alt="" className="w-[200px] h-[200px]" />
+        <img src={album15} alt="" className="w-[200px] h-[200px]" />
+        <img src={album16} alt="" className="w-[200px] h-[200px]" />
+        <img src={album17} alt="" className="w-[200px] h-[200px]" />
+        <img src={album18} alt="" className="w-[200px] h-[200px]" />
+        <img src={album19} alt="" className="w-[200px] h-[200px]" />
+        <img src={album20} alt="" className="w-[200px] h-[200px]" />
+        <img src={album21} alt="" className="w-[200px] h-[200px]" />
+        <img src={album22} alt="" className="w-[200px] h-[200px]" />
+        <img src={album23} alt="" className="w-[200px] h-[200px]" />
+        <img src={album24} alt="" className="w-[200px] h-[200px]" />
+        <img src={album25} alt="" className="w-[200px] h-[200px]" />
+      </div>
+    </div>
+  );
+}
